f572f54aae3c98d63cfcf083d2a2f9370f97c218
Neckron <agpascal@yandex.ru>
2016-10-15 17:55:37 +0300

    first day work first day of work. Spent 3 o'clock

---
+++ Landing_turquoise/js/my_scroll.js
@@ -0,0 +1,28 @@
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+$(document).ready(function(){
+    // = Вешаем событие прокрутки к нужному месту
+    	//	 на все ссылки якорь которых начинается на #
+    	$('a[href^="#"]').bind('click.smoothscroll',function (e) {
+    		e.preventDefault();
+    
+    		var target = this.hash,
+    		$target = $(target);
+    
+    		$('html, body').stop().animate({
+    			'scrollTop': $target.offset().top
+    		}, 900, 'swing', function () {
+    			window.location.hash = target;
+    		});
+    	});
+    
+    });
