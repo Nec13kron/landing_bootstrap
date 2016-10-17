6d101a1b6b09337db927453a071c144bcf3402d1
Neckron <agpascal@yandex.ru>
2016-10-17 14:14:53 +0300

    images compressed

--- Landing_turquoise/js/my_scroll.js
+++ Landing_turquoise/js/my_scroll.js
@@ -1,32 +1,31 @@
-
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
+  target: '.navbar-fixed-top'
 })
 $('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+  $('.navbar-toggle:visible').click();
 });
 
-$(document).ready(function(){
-    // = Вешаем событие прокрутки к нужному месту
-    	//	 на все ссылки якорь которых начинается на #
-    	$('a[href^="#"]').bind('click.smoothscroll',function (e) {
-    		e.preventDefault();
-    
-    		var target = this.hash,
-    		$target = $(target);
-    
-    		$('html, body').stop().animate({
-    			'scrollTop': $target.offset().top
-    		}, 900, 'swing', function () {
-    			window.location.hash = target;
-    		});
-    	});
-    
+$(document).ready(function() {
+  // = Вешаем событие прокрутки к нужному месту
+  //	 на все ссылки якорь которых начинается на #
+  $('a[href^="#"]').bind('click.smoothscroll', function(e) {
+    e.preventDefault();
+
+    var target = this.hash,
+      $target = $(target);
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function() {
+      window.location.hash = target;
     });
-$(window).scroll(function(){
-                  if ($(this).scrollTop() > 50) {
-                      $('.navbar-inverse').addClass('navbar-scroll');
-                  } else if ($(this).scrollTop() < 50) {
-                      $('.navbar-inverse').removeClass('navbar-scroll');
-                  }
-                });
+  });
+
+});
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 50) {
+    $('.navbar-inverse').addClass('navbar-scroll');
+  } else if ($(this).scrollTop() < 50) {
+    $('.navbar-inverse').removeClass('navbar-scroll');
+  }
+});
